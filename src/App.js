28046f2cc16230a7d75851398378e3194dc1a612
caleb <calebp2@illinois.edu>
2021-07-02 11:52:50 -0500

    dump

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
 
 function App() {
   return (
+      <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Nav />
       </header>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+function Nav(){
+    return (
+        <div className="nav-bar" style={{display: "block"}}>
+            <h1>Caleb Patton</h1>
+            <Nav_button name="about"/>
+        </div>
+    );
+}
+
+function Nav_button(props){
+    return (
+        <div className="nav-button">
+            <Link to={'/' + props.name}  style={{ textDecoration: 'none' }}>
+                <li>{props.name}</li>
+            </Link>
+        </div>
+    );
+}
+
+
+
