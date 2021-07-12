b6514777c4ab97e804dc0b11ac1e5fbdc16965d6
calebp2 <calebp2@illinois.edu>
2021-07-12 17:10:42 -0500

    begin projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,57 @@
+ import linkedin from './resources/linkedin.png'
+
 function Projects(){
     return (
-        <div>
-            <p> 
-                This page will contain descriptions of personal projects that I have worked on    
-            </p>
+        <div className='grid-container'>
+            <div className='grid-item'>
+                <ProjectCard/>  
+            </div>
+
+            <div className='grid-item'>
+                <ProjectCard/>  
+            </div>
+
+            <div className='grid-item'>
+                <ProjectCard/>  
+            </div>
+
+            <div className='grid-item'>
+                <ProjectCard/>  
+            </div>
+
+            <div className='grid-item'>
+                <ProjectCard/>  
+            </div>
+
         </div>
     );
 }
 
 export default Projects;
+
+
+/*
+Image
+Title
+Subtitle
+date
+tools
+
+*/
+
+
+function ProjectCard(){
+    return(
+        <div className="card">
+    <a href="/posts/test.html">
+        <img className="card-img-top" src={linkedin} alt="Card cap" height="200px"/>
+            <div className="card-body">
+                <h5 className="card-title" id="card-title">Umpire Consistency Scores (UCS 1.0)</h5>
+                    <p className="card-text" id="card-subtitle">A new measure of umpire consistency for UmpScorecards.</p>
+                        <p className="card-text" id="card-date"><small class="text-muted">Apr 2nd, 2021</small></p>
+            </div>
+    </a>
+</div>
+
+    );
+}
