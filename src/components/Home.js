4a9689d8e8069bdd8e2cea2e8751e6785dc25608
caleb <calebpatton09@gmail.com>
2023-01-21 12:48:28 -0800

    feat: cleaned up small screen formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,43 +11,39 @@ function Home(){
         <div className='homepage'>
             <Container fluid>
                 <Row xs={1} md={2} className='align-items-center'>
-                    <Col>
+                    <Col align='center'>
                         <div className='image'>
                             <Image src={headshot} alt="headshot" roundedCircle/>
                         </div>
                     </Col>
                     <Col>
                         <div className='intro-title'>
-                            <div className='hello'>
-                                Hi, 
-                            </div>
-                            <div className='my-name-is'>
-                                I'm Caleb.
-                            </div>
+                                <div>Hi,</div>
+                                <div>I'm Caleb.</div>
                         </div>
                     </Col>
                 </Row>
                 <Row xs={1} md={2} className='align-items-center'>
-                    <Col>
+                    <Col xs={{order: 1}}>
                         <div className='text-content'>
                             In May 2022 I graduated from the University of Illinois with a B.S. in Computer Engineering. During my 
                             time at UIUC, I explored my interest in Deep Learning and Computer Vision through my coursework and 
                             through internships with three different companies.
                         </div>
                     </Col>
-                    <Col align='center'>
+                    <Col align='center' xs={{order: 2}}>
                         <div className='logo'>
                             <Image src={blockI} alt="uiucLogo" width={250}/>
                         </div>
                     </Col>
                 </Row>
-                <Row xs={1} md={2} className='align-items-center'>
-                    <Col align='center'>
+                <Row xs={1} sm={2} className='align-items-center'>
+                    <Col align='center' xs={{order: 2}} md={{order: 1}}>
                         <div className='logo'>
-                            <Image src={blockN} alt="unrLogo" width={500}/>
+                            <Image src={blockN} alt="unrLogo" width={250}/>
                         </div>
                     </Col>
-                    <Col>
+                    <Col xs={{order: 1}} md={{order: 2}}>
                         <div className='text-content'>
                             After graduation, I moved to Reno, Nevada to start an M.S. in Mechanical Engineering 
                             and to research Autonomous Vehicles in the Learning Autonomy 
