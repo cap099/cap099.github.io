b513f8217c0f20c100b2c139370c48af3dc79911
caleb <calebpatton09@gmail.com>
2023-01-21 16:07:20 -0800

    feat: removed project article functionality

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,47 +1,32 @@
 import projects from './resources/projects/project_info.js'
 import {Card, Container, Row, Col} from 'react-bootstrap'
-import {Link, Route, Switch, useParams, useRouteMatch } from 'react-router-dom'
 
 function Projects(){
-    let route = useRouteMatch();
-
     return (
         <div>
-        <Switch>
-            <Route exact path={route.path}>
-                <div className='project-header'>Some of my past projects:</div>
-                    <div className='project-list'>
-                    <Container>
-                        <Row xl={4}>
-                            {projects.map((project) => (<Col align='center'><ProjectCard    title = {project.title} 
-                                                                        subtitle = {project.subtitle} 
-                                                                        date = {project.date}
-                                                                        tools = {project.tools}
-                                                                        image = {project.image}
-                                                                        link = {project.link}/></Col>))}
-                        </Row>
-                    </Container>
-                </div>
-            </Route>
-            <Route path={`${route.path}/:articleName`}>
-                <Article/>
-            </Route>
-        </Switch>
+            <div className='project-header'>
+                Some of my past projects:
+            </div>
+            <div className='project-list'>
+                <Container>
+                    <Row xl={4}>
+                        {projects.map((project) => (<Col align='center'><ProjectCard    title = {project.title} 
+                                                                    subtitle = {project.subtitle} 
+                                                                    date = {project.date}
+                                                                    tools = {project.tools}
+                                                                    image = {project.image}
+                                                                    link = {project.link}/></Col>))}
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
 
 export default Projects;
 
-function Article(){
-    let {articleName} = useParams();
-    var article = require('./resources/projects/' + articleName + '/' + articleName + '.js').default;
-    return (article);
-}
-
 
 function ProjectCard(props){
-    let route = useRouteMatch();
     const img = require('./resources/projects/' + props.link + '/img/' + props.image).default;
     var tools = props.tools;
     var tool_string = "";
@@ -53,17 +38,15 @@ function ProjectCard(props){
         }   
     }
     return( 
-        <Link className='project-link' to={route.url + '/' + props.link}>
-            <Card className='project-card' >
-                <Card.Img src={img}/>
-                <Card.Body>
-                <Card.Title>{(props.title).toUpperCase()}</Card.Title>
-                <Card.Text>
-                    <div>{props.date}</div>
-                    <div>{tool_string}</div>
-                </Card.Text>
-                </Card.Body>
-            </Card>
-        </Link>
+        <Card className='project-card' >
+            <Card.Img src={img}/>
+            <Card.Body>
+            <Card.Title>{(props.title).toUpperCase()}</Card.Title>
+            <Card.Text>
+                <div>{props.date}</div>
+                <div>{tool_string}</div>
+            </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
